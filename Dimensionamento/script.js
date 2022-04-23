227a7b142be3088740ecc9808ce629662ae10366
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-23 10:06:46 -0300

    Scroll do Botão em smolf

--- Dimensionamento/script.js
+++ Dimensionamento/script.js
@@ -5,3 +5,17 @@ function subirTela() {
         behavior: "smooth"
     });
 }
+function decidrBotaoScroll() {
+    if (window.scrollY === 0) {
+        //ocultar o botão
+        document.querySelector(".scrollbutton").style.display = "none";
+    } else {
+        //mostrar o botão
+        document.querySelector(".scrollbutton").style.display = "block";
+    }
+}
+window.addEventListener("scroll", decidrBotaoScroll);
+/* Essa não e a melhor alternativa para resolver o problema pois sempre
+vai estar verificando o que pode ocasionar demora no site vamos para a outra solucção
+mais efetiva
+setInterval(decidrBotaoScroll, 1000); */
