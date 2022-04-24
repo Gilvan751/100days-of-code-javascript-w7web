f28b6ad578066bf8eda3d2691d7ede058281cf3a
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-24 11:16:25 -0300

    Função que verifica a entrada de dados e utiliza o if como condição de analise

---
+++ Tornando o programa mais acessivel/script.js
@@ -0,0 +1,9 @@
+var idade = prompt("entre com sua idade: ");
+//var genero = prompt("Qual seu genero? M ou F ");
+if (idade >= 18) {
+    //document.write("Pode entrar a pessoa");
+    alert("Pode liberar a entrada ");
+} else {
+    //document.write("Não é permitido sua entrada");
+    alert("Não pode se liberado sua entrada\npois você tem menos de 18 anos");
+}
