6dd30f94974091fa271ab03e1e24c04ad5aa6b1f
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-24 10:44:53 -0300

    Resultado do desafio operação de adição

--- primeiros comandos/script.js
+++ primeiros comandos/script.js
@@ -12,4 +12,9 @@ alert("Seu numero da sorte e: " + x);
 console.log(x); */
 // função confirm(); irmã da função prompt();
 // o resultado da função confirm sempre vai ser booleano true or false
-var y = confirm("Seu numero da sorte é 13: ");
+//var y = confirm("Seu numero da sorte é 13: ");
+// desafio a palavra Number tem que ser escrita em maiuscula
+var x = prompt("Entre com o primeiro numero: ");
+var y = prompt("Entre com o segundo numero: ");
+var soma = Number(x) + Number(y);
+alert("a soma da operação é: " + soma);
