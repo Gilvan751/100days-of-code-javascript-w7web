8a6b3c0ffb22de79eea8084f0013876687d9aa0c
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-24 11:25:06 -0300

    Desafio com o IF

--- Tornando o programa mais acessivel/script.js
+++ Tornando o programa mais acessivel/script.js
@@ -1,4 +1,4 @@
-var idade = prompt("entre com sua idade: ");
+/* var idade = prompt("entre com sua idade: ");
 //var genero = prompt("Qual seu genero? M ou F ");
 if (idade >= 18) {
     //document.write("Pode entrar a pessoa");
@@ -6,4 +6,12 @@ if (idade >= 18) {
 } else {
     //document.write("Não é permitido sua entrada");
     alert("Não pode se liberado sua entrada\npois você tem menos de 18 anos");
+} */
+
+//desafio
+var numero = prompt("Digite um numero: ");
+if (numero >= 10) {
+    alert("Parabéns! este e seu numero da sorte");
+} else {
+    alert("Que pena tente novamente!");
 }
