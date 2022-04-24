92790d32e1371e69fa2a224dde96c736d60eee30
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-24 10:35:52 -0300

    Primeiros comandos no JavaScript

---
+++ primeiros comandos/script.js
@@ -0,0 +1,15 @@
+//console.log();  serve para debugar o programa
+/* var x = 10;
+console.log(x); */
+
+// alert(); função de alerta, comando intruzido
+//alert("Bom dia");
+
+// função prompt(); é a função que captura dados do usuário e armazena
+//na memoria para ser apresentado em uma outra chamda da função
+/* var x = prompt("Digite seu numero da sorte: ");
+alert("Seu numero da sorte e: " + x);
+console.log(x); */
+// função confirm(); irmã da função prompt();
+// o resultado da função confirm sempre vai ser booleano true or false
+var y = confirm("Seu numero da sorte é 13: ");
