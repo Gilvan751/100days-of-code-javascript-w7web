3c90dc3c09fbb409cbe16bdb9c06fa7a74a0dd1e
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-25 11:40:45 -0300

    Funçao recursiva

--- Funções para otimização do código/script.js
+++ Funções para otimização do código/script.js
@@ -18,10 +18,20 @@ soma(); */
 }
 plin(); */
 // temos que fazer micro funções para execultar em uma função que somente chama a função principal para facilitar o codigo
-function soma(x, y) {
+/* function soma(x, y) {
     return x + y;
 }
 var v1 = Number(prompt("Digite o primeiro valor"));
 var v2 = Number(prompt("Digite o segundo valor"));
 
-alert("Resultado e: " + soma(v1, v2));
+alert("Resultado e: " + soma(v1, v2)); */
+function potencia(base, expoente) {
+ if (expoente == 0) {
+ return 1;
+ } else {
+ return base * potencia(base, expoente - 1); // nessa parte a recursão
+//dá lugar ao for
+ }
+}
+console.log(potencia(2, 3));
+// → 8
