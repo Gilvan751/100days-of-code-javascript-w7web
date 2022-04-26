cdbf03d74a3ebd8f5c1b328283e9ffe4436ec74d
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-26 11:46:53 -0300

    document.getElementById e document.getElementsByClasseName

--- DOM/script.js
+++ DOM/script.js
@@ -1,1 +1,5 @@
-document.write("Essa e nova");
+//document.write("Essa e nova");//esse escreve na tela 
+console.log(document.getElementById("time"));
+console.log(
+    document.getElementsByClassName("urso"));
+   
