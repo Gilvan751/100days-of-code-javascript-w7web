5842c47b7467bb08d4708fe725d0bbf050626c8f
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-26 21:22:35 -0300

    CreateElement

--- document.querySelector/script.js
+++ document.querySelector/script.js
@@ -1,5 +1,5 @@
 
-console.log(
+/* console.log(
     document.querySelector("p.teste3") 
 )
 console.log(document.querySelector("#teste1"))
@@ -7,6 +7,13 @@ console.log(document.querySelector(".teste2"))
 console.log(document.querySelectorAll(".teste2"))
 console.log(document.querySelectorAll("#teste1"))
 console.log(document.querySelectorAll("p"))
-console.log(document.getElementById("pai").querySelectorAll(".filho"))
+console.log(document.getElementById("pai").querySelectorAll(".filho")) */
+var img = document.createElement("img")
+img.src = "https://www.fillmurray.com/640/360";
+
+
+console.log(
+    img
+)
 
 
