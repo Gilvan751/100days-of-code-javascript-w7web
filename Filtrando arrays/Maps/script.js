34fd81176b000b2631fcd2e0d660be5b1c2d032e
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-27 18:01:49 -0300

    Novas funções

--- Filtrando arrays/Maps/script.js
+++ Filtrando arrays/Maps/script.js
@@ -1,6 +1,6 @@
 var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var multiplica = function(item) {
- return item * 2;
+ return item * 7;
 }
 var dobrados = numeros.map(multiplica);
 console.log(dobrados);
@@ -20,3 +20,17 @@ var letras = function (item) {
 var letras = letra.map(letras);
 
 console.log(letras);
+
+var fahrenheit = [0, 32, 45, 46, 47, 91, 93, 121];
+var celcius = fahrenheit.map(function(item) {
+ return Math.round((item - 32)*5/9);
+});
+console.log(celcius);
+// → [-18, 0, 7, 8, 8, 33, 34, 49]
+
+
+var cel = [-18, 0, 7, 36,  8, 8, 33, 34, 49];
+var far = cel.map(function (item) {
+    return Math.floor((item * 1.8) + 32);
+});
+console.log(far);
