d2ae62a3b9232fe15cc12745b9646247d7db814d
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-26 21:09:18 -0300

    Document Query Selector e Query selector All

---
+++ document.querySelector/script.js
@@ -0,0 +1,12 @@
+
+console.log(
+    document.querySelector("p.teste3") 
+)
+console.log(document.querySelector("#teste1"))
+console.log(document.querySelector(".teste2"))
+console.log(document.querySelectorAll(".teste2"))
+console.log(document.querySelectorAll("#teste1"))
+console.log(document.querySelectorAll("p"))
+console.log(document.getElementById("pai").querySelectorAll(".filho"))
+
+
