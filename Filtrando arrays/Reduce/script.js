f40406e9c01daa0a135b91bc06b47768f1d86c92
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-04-29 16:55:37 -0300

    Estudo de Reduce completo

--- Filtrando arrays/Reduce/script.js
+++ Filtrando arrays/Reduce/script.js
@@ -11,3 +11,10 @@ var cal = function (total, item) {
 var somatorias = valore.reduce(cal);
 var media = somatorias / valore.length;
 console.log(media);
+
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var dobrados = numeros.reduce(function(total, item) {
+ return total.concat(item * 2);
+}, []);
+console.log(dobrados);
+// → [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
