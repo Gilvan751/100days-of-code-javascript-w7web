d1315d53372082e1b02d9e0dee32761e3d11a56f
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-05 13:52:28 -0300

    Operação de toFixed(),replace e toLocaleString para mudar a moeda de cada pais

--- primeiros comandos/script.js
+++ primeiros comandos/script.js
@@ -14,7 +14,20 @@ console.log(x); */
 // o resultado da função confirm sempre vai ser booleano true or false
 //var y = confirm("Seu numero da sorte é 13: ");
 // desafio a palavra Number tem que ser escrita em maiuscula
-var x = prompt("Entre com o primeiro numero: ");
-var y = prompt("Entre com o segundo numero: ");
+/* var x = Number(prompt("Entre com o primeiro numero: "));
+var y = Number(prompt("Entre com o segundo numero: "));
 var soma = Number(x) + Number(y);
-alert("a soma da operação é: " + soma);
+alert("a soma da operação é: " + soma); */
+/* var nome = prompt('Qual é seu nome'.toUpperCase())
+document.write(`Ola,<strong>${nome}</strong>! Seu nome tem ${nome.length}letras <br/>`)
+document.write(`Seu nome em maiúscula é ${nome.toUpperCase()} <br/>`)
+document.write(`Seu nome em minuscula é ${nome.toLowerCase()}`) */
+/* n1.toFixed(2) com duas casas decimais apos a virgula
+n1.toFixed(2).replace('.',',') replace troca o ponto pela virgulan1.to 
+n1.toLocaleString('pt-BR',{style:'currency',currency:'BRL'}) COMANDO PARA COLOCAR A MOEDDAA 
+EM QUE TIPO DE PAIS*/
+
+var n1 = 1543.5
+document.write(`${n1.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}<br/>`)
+document.write(`${n1.toLocaleString('pt-BR',{style:'currency',currency:'EUR'})}<br/>`)
+document.write(`${n1.toLocaleString('pt-BR',{style:'currency',currency:'USD'})}`)
