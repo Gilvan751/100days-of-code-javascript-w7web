354302e3146cdecc7b70bd895390f3c36b185aab
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-04 21:03:15 -0300

    Exercicios sobre tipos de dados

---
+++ Tipos de dados/Aula 06/script.js
@@ -0,0 +1,13 @@
+/* 
+var nome = prompt('Qual é seu nome? ')
+alert(`Muito bem ${nome} você foi selecionado para uma bolsa de estudos`)
+/* isso e um comentario */
+/* var idade = prompt('Digite sua idade: ')
+alert(`Muito bem ${nome} você só tem ${idade} anos, esta muito novo`)
+var numero = Number(prompt('Digite um numero '))
+var numero2 = Number(prompt('Digite um numero '))
+var soma = numero + numero2
+alert(`A soma de ${numero} com ${numero2} é igual a ${soma}`)
+ */
+var s = 'JavaSecript'
+alert(`o valor armazenado na variavel s é ${s}`)
