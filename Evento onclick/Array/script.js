6b22575fc8c4464479109af887da8be65e061465
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-07 21:03:19 -0300

    Projeção de elementos de um array

---
+++ Evento onclick/Array/script.js
@@ -0,0 +1,9 @@
+let carros = ['Palio', 'Uno', 'Corolla', 'Ferrari'];
+console.log(carros[0]);
+document.write(carros[7]);
+
+let ingredientes = [
+    ['uva', 'pera', 'maça'],
+    ['arroz', 'macarrão']
+];
+console.log(ingredientes[1][0]);
