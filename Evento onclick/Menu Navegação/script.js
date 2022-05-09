b3a43a0cbd7170984a932b083be455f57aba729f
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-09 13:36:23 -0300

    Primeira maneira de utilizar a navegação do menu

--- Evento onclick/Menu Navegação/script.js
+++ Evento onclick/Menu Navegação/script.js
@@ -1,10 +1,11 @@
 function menuToggle() {
     let menuArea = document.getElementById('menu-area');
-    if (menuArea.style.width == '200px') {
-        menuArea.style.width = '0px';
+    if (menuArea.classList.contains('menu-opened') == true) {
+        menuArea.classList.remove('menu-opened');
     } else {
-        menuArea.style.width = '200px';
+        menuArea.classList.add('menu-opened');
     }
+    
 }
 
 //primeira maneira de fazer com que o menu abra e feche lembrando que tem que usar a classe do css
@@ -14,3 +15,8 @@ function menuToggle() {
         menuArea.classList.add('menu-opened');
     } */
     //segunda maneira de resolver e a mais simples que eu achei a que esta acima, porem a primeira e a mais refinada
+    /* if (menuArea.style.width == '200px') {
+        menuArea.style.width = '0px';
+    } else {
+        menuArea.style.width = '200px';
+    } */
